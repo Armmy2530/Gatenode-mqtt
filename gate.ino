ce9c7abfcc0d15c4f739d0316394a5d20a84c96f
Armmy2530 <sutigran2557@gmail.com>
2021-10-22 20:12:03 +0700

    add device connection and mode

--- gate.ino
+++ gate.ino
@@ -2,6 +2,7 @@
 #include <PubSubClient.h>
 #include <DNSServer.h>
 #include <WiFiManager.h>
+#include <EEPROM.h>
 
 const char* ssid         = "floor_1";
 const char* password     = "83005097";
@@ -9,9 +10,12 @@ const char* mqttServer   = "192.168.2.56";
 const int  mqttPort     = 1883;
 const char* mqttUser     = "armmy2530";
 const char* mqttPassword = "arm254889";
-
-#define PUB_GPIO2_STATUS "cmnd/gatenode/status"
-#define SUB_GPIO2_ACTION "cmnd/gatenode/cmnd"
+unsigned long int currentTime = millis();
+unsigned long int previousTime = 0;
+String currentMode = "online";
+#define PUB_GPIO2_CONNECTION "tele/gatenode/LWT"
+#define PUB_GPIO2_MODE "stat/gatenode/mode"
+#define SUB_GPIO2_ACTION "cmnd/gatenode"
 #define GPIO2_LED 3
 
 WiFiClient wifiClient;
@@ -53,15 +57,14 @@ void initWifiStation() {
   mqttUser = custom_mqtt_username.getValue();
   mqttPassword = custom_mqtt_password.getValue();
   delay(1000);
-  
-  delay(100);
   mqttClient.setServer(mqttServer, mqttPort);
   mqttClient.setCallback(PubSubCallback);
   while (!mqttClient.connected()) {
     delay(100);
     Serial.println("Connecting to MQTT (" + String(mqttServer) + ") ...");
-    if (mqttClient.connect("Gatenode_mk3", mqttUser, mqttPassword)) {
+    if (mqttClient.connect("Gatenode_mk3", mqttUser, mqttPassword,PUB_GPIO2_CONNECTION,0,true,"Offline")) {
       Serial.println("MQTT client connected");
+      mqttClient.publish(PUB_GPIO2_CONNECTION, "Online",true);
     } 
     else {
       Serial.print("\nFailed with state ");
@@ -73,16 +76,17 @@ void initWifiStation() {
     delay(2000);
   }
   // Declare Pub/Sub topics
-  mqttClient.publish(PUB_GPIO2_STATUS, "off");
   mqttClient.subscribe(SUB_GPIO2_ACTION);
 }
 
 
 void PubSubCallback(char* topic, byte* payload, unsigned int length) {
   String strTopicGpio2Action = SUB_GPIO2_ACTION;
+  String strON = "active";
+  String strOFF = "deactive";
+  String strOnline = "online";
+  String strOffline = "offline";
   String strPayload = "";
-  String strON = "on";
-  String strOFF = "off";
   Serial.print("Topic:");
   Serial.println(topic);
   Serial.print("Message:");
@@ -94,19 +98,43 @@ void PubSubCallback(char* topic, byte* payload, unsigned int length) {
   Serial.println("-----------------------");
 
   if (strTopicGpio2Action == topic) {
-    if (strON == strPayload) {
-      mqttClient.publish(PUB_GPIO2_STATUS, "on");
+    if (strON == strPayload && currentMode == strOnline) {
       digitalWrite(GPIO2_LED, HIGH);
       delay(500);
       digitalWrite(GPIO2_LED, LOW);
       Serial.println("gate opening");
-      mqttClient.publish(PUB_GPIO2_STATUS, "off");
-    } else if (strOFF == strPayload) {
+    } 
+    else if (strON == strPayload && currentMode == strOffline) {
+      Serial.println("Current mode is offline");
+    } 
+    else if (strOFF == strPayload) {
       digitalWrite(GPIO2_LED, LOW);
+    }
+    else if (strOnline == strPayload)
+    {
+      currentMode = strOnline;
+      Serial.println("Current mode has set to " + currentMode);
+      mqttClient.publish(PUB_GPIO2_MODE, "online");
+    }
+    else if (strOffline == strPayload)
+    {
+      currentMode = strOffline;
+      Serial.println("Current mode has set to " + currentMode);
+      mqttClient.publish(PUB_GPIO2_MODE, "offline");
+    }
+    else if (strPayload == "wifireset"){
+      Serial.println("reset config");
+      wifiManager.resetSettings();
+      delay(100);
+      ESP.reset();
+    }
+    else if (strPayload == "restart"){
+      Serial.println("restart");
+      delay(100);
+      ESP.reset();
     }
   }
 }
-String serial_raw ;
 void setup() {
   Serial.begin(115200);
   pinMode(GPIO2_LED, OUTPUT);
@@ -117,4 +145,5 @@ void setup() {
 void loop() {
   mqttClient.loop();
   delay(100);
+  currentTime = millis();
 }
